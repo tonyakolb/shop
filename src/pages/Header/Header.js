d43c77947e33ce4ab2c1a9086842c2fdd5dfd9cd
Alexandra Astroukh <astraukh02@gmail.com>
2022-11-29 20:04:16 +0300

    heeeder

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -2,20 +2,27 @@ import React from 'react';
 import Input from '../../components/Input/Input';
 // import Button from '../../components/Button/Button';
 import Logo from '../../components/Header/Logo/Logo';
+//import SearchIcon from '../../assets/images/search-icon.svg';
 import './Header.css';
 
 const Header = () => {
     return (
-        <header>
-            <Logo/>
-            <Input/>
-            <a className='' >Каталог</a>
-            <a className=''>Акции</a>
-            <a>Доставка</a>
-            <a className=''>Контакты</a>
-            <a>Иконка</a>
-            <p>+375</p>
-            <p>Время работы</p>
+        <header className='header'>
+            <Logo />
+            <div className=''>
+                
+                </div>
+            <Input className='header-search' />
+            <a href='/' className='' >Каталог</a>
+            <a href='/' className=''>Акции</a>
+            <a href='/'>Доставка</a>
+            <a href='/' className=''>Контакты</a>
+            <a href='/'>Иконка</a>
+            <p className='header-phone'>+375 (44) 584 20 68</p>
+            <div className='header-schedule'>
+                <p>Вт-Вс: 10:00 - 20:00</p>
+                <p>Пн: выходной</p>
+             </div>
         </header>
     );
 };
