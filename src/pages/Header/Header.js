893877496b3ac9ec4525dae8979acf280d3b5b7f
tonyakolb <antoninakolb@gmail.com>
2022-11-29 20:40:56 +0300

    icon basket

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -1,32 +1,43 @@
-import React from 'react';
-import Input from '../../components/Input/Input';
-import Logo from '../../components/Header/Logo/Logo';
-import Icon from '../../assets/images/Icon-basket.svg';
-import './Header.css';
+import React from "react";
+import Input from "../../components/Input/Input";
+import Logo from "../../components/Header/Logo/Logo";
+import Icon from "../../assets/images/Icon-basket.svg";
+import SearchIcon from '../../assets/images/search-icon.svg';
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <header className='header'>
-            <Logo />
-            <div className=''>
-                
-                </div>
-            <Input className='header-search' />
-            <a href='/' className='' >Каталог</a>
-            <a href='/' className=''>Акции</a>
-            <a href='/'>Доставка</a>
-            <a href='/' className=''>Контакты</a>
-            <a href='/'>
-                <div><img src={Icon} alt="basket" />
-                    <div></div></div>
-            </a>
-            <p className='header-phone'>+375 (44) 584 20 68</p>
-            <div className='header-schedule'>
-                <p>Вт-Вс: 10:00 - 20:00</p>
-                <p>Пн: выходной</p>
-             </div>
-        </header>
-    );
+  return (
+    <header className="header">
+      <Logo />
+      <div className="">
+          <img src={SearchIcon} alt="search-icon"/>
+      </div>
+      <Input className="header-search" />
+      <a href="/" className="">
+        Каталог
+      </a>
+      <a href="/" className="">
+        Акции
+      </a>
+      <a href="/">Доставка</a>
+      <a href="/" className="">
+        Контакты
+      </a>
+      <a href="/">
+        <div>
+          <img src={Icon} alt="basket" />
+          <div>
+              <span></span>
+          </div>
+        </div>
+      </a>
+      <p className="header-phone">+375 (44) 584 20 68</p>
+      <div className="header-schedule">
+        <p>Вт-Вс: 10:00 - 20:00</p>
+        <p>Пн: выходной</p>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
