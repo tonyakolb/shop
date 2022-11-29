5b33aac7f2593876ce9ceb4a873f285a076f81d6
tonyakolb <antoninakolb@gmail.com>
2022-11-29 20:29:09 +0300

    icon basket

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Input from '../../components/Input/Input';
-// import Button from '../../components/Button/Button';
 import Logo from '../../components/Header/Logo/Logo';
+import Icon from '../../assets/images/Icon-basket.svg';
 import './Header.css';
 
 const Header = () => {
@@ -13,7 +13,10 @@ const Header = () => {
             <a className=''>Акции</a>
             <a>Доставка</a>
             <a className=''>Контакты</a>
-            <a>Иконка</a>
+            <a>
+                <div><img src={Icon} alt="basket"/>
+                <div></div></div>
+            </a>
             <p>+375 (44) 584 20 68</p>
             <p>Вт-Вс: 10:00 - 20:00</p>
             <p>Пн: выходной</p>
