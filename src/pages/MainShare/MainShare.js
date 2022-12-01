a828818faf3981b275adbc74af56fb2f57ad6eb1
Alexandra Astroukh <astraukh02@gmail.com>
2022-12-01 17:56:55 +0300

    mainn

--- src/pages/MainShare/MainShare.js
+++ src/pages/MainShare/MainShare.js
@@ -1,15 +1,16 @@
 import React from "react";
 import MainImage from '../../assets/images/main-image.jpg';
-
+import "./MainShare.css";
 
 const MainShare = () => {
-  return (
-    <div>
-      <div>
-        <div>
-          <p>Акция Все лето скидка 10% на весь ассортимент продукции</p>
+    return (
+        <div className='main'>
+            <div className='main-share'>
+                <div className='main-share-text'>
+                    <p>Акция</p>
+                    <p>Все лето скидка 10% на весь ассортимент продукции</p>
         </div>
-        <div>
+                <div className='main-share-img'>
           <img src={MainImage} alt='main-image'/>
         </div>
       </div>
